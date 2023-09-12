644fe200696c72d05c68dc782be6eef8bf9071d3
Louie <sxl7953@rit.edu>
2023-09-12 18:43:41 -0400

    got initial data to load on mount

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,32 @@ import axios from "axios";
 
 function App() {
   const apiKey = import.meta.env.VITE_APP_API_KEY;
+
+  const [myData, setmyData] = useState({});
+  const [isLoading, setisLoading] = useState(false);
+
+  const testAPI = async (isp) => {
+    setisLoading(true);
+    const response = await axios.get(
+      `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${isp}`
+    );
+    setmyData(response.data);
+  };
+
+  useEffect(() => {
+    testAPI("");
+    setisLoading(false);
+  }, []);
+
+  console.log(myData);
+
   return (
     <>
       <main className="font-primary min-h-screen bg-[url('src/assets/pattern-bg-desktop.png')] bg-auto xl:bg-cover">
         <Header />
         <SearchBar />
         <EntireMap />
-        <Info />
+        <Info getLocation={myData} checkLoading={isLoading} />
       </main>
     </>
   );
--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,30 +1,45 @@
-export default function Info() {
+export default function Info({ getLocation, checkLoading }) {
+  const { ip, location, isp } = getLocation;
+
+  const FinishedLoading = () => {
+    if (!location) {
+      return <div>Loading...</div>;
+    }
+    return (
+      <>
+        <div className="flex flex-col px-4 mx-auto mb-10 mt-4 ">
+          <p className="text-[#969696] font-medium tracking-widest py-2 text-sm">
+            IP ADDRESS
+          </p>
+          <p className="text-[#2b2b2b] font-bold">{ip}</p>
+        </div>
+        <div className="flex flex-col px-4 mx-auto mb-10 mt-4 ">
+          <p className="text-[#969696] font-medium tracking-widest py-2 text-sm">
+            LOCATION
+          </p>
+          <p className="text-[#2b2b2b] font-bold">
+            {location.city}, {location.region} {location.postalCode}
+          </p>
+        </div>
+        <div className="flex flex-col px-4 mx-auto mb-10 mt-4">
+          <p className="text-[#969696] font-medium tracking-widest py-2 text-sm">
+            TIMEZONE
+          </p>
+          <p className="text-[#2b2b2b] font-bold">UTC {location.timezone}</p>
+        </div>
+        <div className="flex flex-col px-4  mx-auto mb-10 mt-4">
+          <p className="text-[#969696] font-medium tracking-widest py-2 text-sm">
+            ISP
+          </p>
+          <p className="text-[#2b2b2b] font-bold">{isp}</p>
+        </div>
+      </>
+    );
+  };
+
   return (
     <section className="flex flex-col md:flex-row  md:divide-x md:divide-[#d8d8d8] max-w-3xl bg-white rounded-lg text-[18px] mx-auto relative bottom-14 z-10  ">
-      <div className="flex flex-col px-4 mx-auto mb-10 mt-4 ">
-        <p className="text-[#969696] font-medium tracking-widest py-2 text-sm">
-          IP ADDRESS
-        </p>
-        <p className="text-[#2b2b2b] font-bold">192.212.174.101</p>
-      </div>
-      <div className="flex flex-col px-4 mx-auto mb-10 mt-4 ">
-        <p className="text-[#969696] font-medium tracking-widest py-2 text-sm">
-          LOCATION
-        </p>
-        <p className="text-[#2b2b2b] font-bold">Brooklyn, NY 10001</p>
-      </div>
-      <div className="flex flex-col px-4 mx-auto mb-10 mt-4">
-        <p className="text-[#969696] font-medium tracking-widest py-2 text-sm">
-          TIMEZONE
-        </p>
-        <p className="text-[#2b2b2b] font-bold">UTC -05:00</p>
-      </div>
-      <div className="flex flex-col px-4  mx-auto mb-10 mt-4">
-        <p className="text-[#969696] font-medium tracking-widest py-2 text-sm">
-          ISP
-        </p>
-        <p className="text-[#2b2b2b] font-bold">SpaceX Starlink</p>
-      </div>
+      <FinishedLoading />
     </section>
   );
 }
